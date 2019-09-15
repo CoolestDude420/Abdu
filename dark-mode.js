51cf06534759183b9af1dd51b50ad8d12b822e49
CoolestDude420 <47911637+CoolestDude420@users.noreply.github.com>
2019-09-16 11:48:21 +1200

    Add files via upload

---
+++ dark-mode.js
@@ -0,0 +1,20 @@
+// variable ste
+var checkbox = document.querySelector("input[name=theme]");
+// if the switch is checkd the dark theme attritube will change to dark and display a warnig
+checkbox.addEventListener("change", function() {
+	if (this.checked) {
+		trans()
+		document.documentElement.setAttribute("data-theme", "dark")
+		alert("WARNING: \nDark Mode is only experimental and may have performance or visual issues!")
+	} else {
+		trans()
+		document.documentElement.setAttribute("data-theme", "light")
+	}
+})
+// transition for switching to night mode
+let trans = () => {
+	document.documentElement.classList.add("transition");
+	window.setTimeout(() => {
+		document.documentElement.classList.remove("transition")
+	}, 1000)
+}
